ad9e27768a9fc4a29af3ace87acd42ec19e4e9e0
Nitesh Oswal <nit.oswal@gmail.com>
2016-12-10 17:53:43 +0530

    Add some template responses which are to be passed from parent

---
+++ templates.py
@@ -0,0 +1,64 @@
+import inflection
+
+postbacks = {
+    "ML_LOCATION": "ML_LOCATION",
+    "RR_IS_IT_GOOD": "RR_IS_IT_GOOD"
+}
+
+# multiple locations
+def ML(payload):
+    locations = []
+    for location in payload:
+        locations.append({
+            "content_type": "text",
+            "title": location,
+            "payload": postbacks["ML_LOCATION"]
+        })
+    return {
+        "text": "So, where do you think you are..",
+        "quick_replies": locations
+    }
+
+# not found, payload is a bunch of text from the bot
+def NF(payload):
+    return TX(payload)
+
+# for fetching the location
+def LC(payload):
+    return TX(payload)
+
+def RR(payload):
+    elements = []
+    for element in payload:
+        elements.append({
+            "title": element["name"],
+            "item_url": element["mobile_url"],
+            "image_url": element["image_url"],
+            "subtitle": element["snippet_text"],
+            "buttons": [
+                {
+                    "type": "web_url",
+                    "url": element["mobile_url"],
+                    "title": "Check it out!"
+                },
+                {
+                    "type": "postback",
+                    "title": "Is it good?",
+                    "payload": postbacks["RR_IS_IT_GOOD"]
+                }
+            ]
+        })
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": elements
+            }
+        }
+    }
+
+def TX(payload):
+    return {
+        "text": payload
+    }
