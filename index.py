76ba67166273585db24065280b970823d57c2a23
Nitesh Oswal <nit.oswal@gmail.com>
2016-12-10 11:00:27 +0530

    Lets hook it up!

--- index.py
+++ index.py
@@ -1,2 +1,20 @@
 import web
+import json
 
+urls = (
+    "/hookmeup", "index"
+)
+
+class index(object):
+    def __init__(self):
+        self.requests = []
+
+    def GET(self):
+        web.header("Content-Type", "application/json")
+        web. json.dumps({
+            "status": True
+        })
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
